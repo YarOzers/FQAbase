33a994b203167c1ebf203757d9248ec983be7481
Yarik Ozerskiy <ozerskiy.yaroslav@gmail.com>
2024-04-13 02:04:18 +0400

    add logging form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavigateComponent} from "./views/navigate/navigate.component";
+import { NgbModule} from "@ng-bootstrap/ng-bootstrap";
+import {LogingComponent} from "./views/loging/loging.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LogingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
